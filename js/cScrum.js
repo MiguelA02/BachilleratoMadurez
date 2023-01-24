87d9f9a7a11f10acba7c1ea77b48b5923011c69c
MiguelA02 <101620100+MiguelA02@users.noreply.github.com>
2023-01-23 19:40:28 -0600

    validacion scrum

--- js/cScrum.js
+++ js/cScrum.js
@@ -1,33 +1,45 @@
-let count = 0
 const enviar = () => {
-    console.log(count)
-    return false
-}
+  let count = 0;
 
+  var rad1 = document.myForm1.myRadios1;
+  var rad2 = document.myForm2.myRadios2;
+  var rad3 = document.myForm3.myRadios3;
+  var rad4 = document.myForm4.myRadios4;
+  var rad5 = document.myForm5.myRadios5;
+  var rad6 = document.myForm6.myRadios6;
+  var rad7 = document.myForm7.myRadios7;
+  var rad8 = document.myForm8.myRadios8;
+  var rad9 = document.myForm9.myRadios9;
+  var rad10 = document.myForm10.myRadios10;
+  var rad11 = document.myForm11.myRadios11;
+  var rad12 = document.myForm12.myRadios12;
+  var rad13 = document.myForm13.myRadios13;
+  var rad14 = document.myForm14.myRadios14;
+  var rad15 = document.myForm15.myRadios15;
 
+  const rad = [
+    rad1,
+    rad2,
+    rad3,
+    rad4,
+    rad5,
+    rad5,
+    rad6,
+    rad7,
+    rad8,
+    rad9,
+    rad10,
+    rad11,
+    rad12,
+    rad13,
+    rad14,
+    rad15,
+  ];
 
-var rad = document.myForm1.myRadios1;
-
-for (var i = 0; i < rad.length; i++) {
-    rad[i].addEventListener('change', function() {
-        if(this.value === '1'){
-            count++;
-        }else{
-            count--
-        }
-        
-    });
-}
-
-var rad = document.myForm2.myRadios2;
-
-for (var i = 0; i < rad.length; i++) {
-    rad[i].addEventListener('change', function() {
-        if(this.value === '1'){
-            count++;
-        }else{
-            count--
-        }
-        
-    });
-}
+  for (var i = 0; i < rad.length; i++) {
+    if(rad[i].value === '1'){
+        count++
+    }
+  }
+  console.log(count);
+};
